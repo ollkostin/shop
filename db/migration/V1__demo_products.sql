52f380a158faac0dfac446a41ae2091aa96f886a
Vladimir Kostin <ollkostin@gmail.com>
2017-09-21 17:55:23 +0300

    [persistence] migration with demo products data

---
+++ db/migration/V1__demo_products.sql
@@ -0,0 +1,14 @@
+INSERT INTO product
+VALUES
+  (1, 'product_1', 100, 'very good product 1'),
+  (2, 'product_2', 200, 'very good product 2'),
+  (3, 'product_3', 150, 'very good product 3'),
+  (4, 'product_4', 160, 'very good product 4'),
+  (5, 'product_5', 170, 'very good product 5'),
+  (6, 'product_6', 1500, 'very good product 6'),
+  (7, 'product_7', 100, 'very good product 7'),
+  (8, 'product_8', 200, 'very good product 8'),
+  (9, 'product_9', 150, 'very good product 9'),
+  (10, 'product_10', 160, 'very good product 10'),
+  (11, 'product_11', 170, 'very good product 11'),
+  (12, 'product_12', 1500, 'very good product 12');
