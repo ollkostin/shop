e69a2151e86267f8dcce8217bc81694413628225
Vladimir Kostin <ollkostin@gmail.com>
2017-09-27 11:18:14 +0300

    [web] add mvc config for resources

--- src/main/java/ru/practice/kostin/shop/ShopApplication.java
+++ src/main/java/ru/practice/kostin/shop/ShopApplication.java
@@ -2,10 +2,8 @@ package ru.practice.kostin.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class ShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class);
